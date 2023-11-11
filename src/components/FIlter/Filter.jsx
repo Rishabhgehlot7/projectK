81aaf953bc95ac5bc317eeb608659ad728d98801
rishabh gehlot <rishabh3x@gmail.com>
2023-11-12 02:50:44 +0530

    commit 1

--- src/components/FIlter/Filter.jsx
+++ src/components/FIlter/Filter.jsx
@@ -7,21 +7,21 @@ export default function () {
             <section className='flex items-center justify-between m-auto w-full'>
                 <NavLink to={'/ALL'}
                     className={({ isActive }) =>
-                        `border-[1px] border-black text-center w-[32%] mx-2 my-2 text-xl ${isActive ? "bg-gray-800 text-white" : " text-gray-700"}`
+                        `border-[1px] border-black text-center w-[32%] mx-2 my-2 py-1  text-xl ${isActive ? "bg-gray-800 text-white" : " text-gray-700"}`
                     }
                 >
                     ALL
                 </NavLink>
                 <NavLink to={'/MAINLINE'}
                     className={({ isActive }) =>
-                        `border-[1px] border-black text-center w-[32%] mx-2 my-2 text-xl ${isActive ? "bg-gray-800 text-white" : " text-gray-700"}`
+                        `border-[1px] border-black text-center w-[32%] mx-2 my-2 py-1 text-xl ${isActive ? "bg-gray-800 text-white" : " text-gray-700"}`
                     }
                 >
                     MAINLINE
                 </NavLink>
                 <NavLink to={'/SME'}
                     className={({ isActive }) =>
-                        `border-[1px] border-black text-center w-[32%] mx-2 my-2 text-xl ${isActive ? "bg-gray-800 text-white" : " text-gray-700"}`
+                        `border-[1px] border-black text-center w-[32%] mx-2 my-2 py-1 text-xl ${isActive ? "bg-gray-800 text-white" : " text-gray-700"}`
                     }
                 >
                     SME
